0208d4cac1e99cb5b6667e80c0228f6e3069aead
NayeemJohnY <njnayeem@gmail.com>
2025-06-18 22:38:30 +0530

    Add Day 003

---
+++ src/test/java/gfg/problemsolving/arrays/GFG_003_ReverseAnArray.java
@@ -0,0 +1,30 @@
+package gfg.problemsolving.arrays;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class GFG_003_ReverseAnArray {
+
+    @Test(dataProvider = "arrayData")
+    public void reverseArray(int arr[], int[] expectedOutput) {
+        // code here
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left++] = arr[right];
+            arr[right--] = temp;
+        }
+        Assert.assertEquals(arr, expectedOutput);
+    }
+
+    @DataProvider(name = "arrayData")
+    public Object[][] arrayData() {
+        return new Object[][] {
+                { new int[] { 1, 4, 3, 2, 6, 5 }, new int[] { 5, 6, 2, 3, 4, 1 } },
+                { new int[] { 4, 5, 2 }, new int[] { 2, 5, 4 } },
+                { new int[] { 1 }, new int[] { 1 } }
+        };
+    }
+
+}
